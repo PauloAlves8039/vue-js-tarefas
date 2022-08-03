4e8c007224a843169c5faf3ed9f30801ad3d8981
PauloAlves8039 <pj38alves@gmail.com>
2022-08-03 20:29:38 -0300

    Montando o componente tarefas

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,12 @@
 <template>
-    <div id="Task">
-        <h1>Task</h1>
+    <div id="task">
+        <form>
+            <input 
+            type="text"
+            placeholder="Tarefa de hoje?"
+            >
+            <button type="submit">Adicionar</button>
+        </form>
     </div>
 </template>
 
@@ -12,5 +18,41 @@ export default {
 </script>
 
 <style scoped>
+    #task{
+        max-width: 700px;
+        background: #FFF;
+        border-radius: 4px;
+        padding: 20px;
+        margin: 20px auto;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    }
 
+    form{
+        margin-top: 30px;
+        display: flex;
+        flex-direction: row;
+    }
+
+    form button{
+        cursor: pointer;
+        background: #0F5959;
+        border: 0;
+        border-radius: 4px;
+        margin-left: 10px;
+        padding: 0 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #FFF;
+    }
+    
+    input{
+        flex: 1;
+        border: 1px solid #EEE;
+        padding: 6px 10px;
+        border-radius: 4px;
+        font-size: 14px;
+        outline: none;
+    }
+    
 </style>
